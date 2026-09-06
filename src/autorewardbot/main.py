cc648f583495a735eee68ea5632940ddacdb0677
MakeMeFifi <chrismikolajek2006@gmail.com>
2026-09-07 00:38:31 +0200

    created start

---
+++ src/autorewardbot/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from rich.console import Console
+import pyfiglet
+
+console: Console = Console()
+title = pyfiglet.figlet_format("Auto Reward Bot", font="pagga")
+def main():
+    console.print(f"[bold green] {title} [/bold green] \n [bold yellow] created by MakeMeFifi [/bold yellow]")
+
+    
--- src/autorewardbot/test.py
+++ src/autorewardbot/test.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 
-driver:webdriver.Firefox = webdriver.Firefox()
+driver:webdriver.Edge = webdriver.Edge()
+testDrive:webdriver.Firefox = webdriver.Firefox()
 
 def main() ->None :
     driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-    print(driver.title)
+    testDrive.get("https://www.selenium.dev/selenium/web/web-form.html")
+    print(f"{driver.title} is from edge and \n {testDrive.title} is from firefox")
