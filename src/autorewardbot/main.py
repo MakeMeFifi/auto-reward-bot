ac5db96f33b94bfd00f0b3f207bb56f0c0d6e047
MakeMeFifi <chrismikolajek2006@gmail.com>
2026-09-07 00:38:34 +0200

    Updated main

--- src/autorewardbot/main.py
+++ src/autorewardbot/main.py
@@ -4,7 +4,9 @@ import pyfiglet
 
 console: Console = Console()
 title = pyfiglet.figlet_format("Auto Reward Bot", font="pagga")
+driver:webdriver.Edge = webdriver.Edge()    # Opens the edge window
+
+
 def main():
     console.print(f"[bold green] {title} [/bold green] \n [bold yellow] created by MakeMeFifi [/bold yellow]")
 
-    
