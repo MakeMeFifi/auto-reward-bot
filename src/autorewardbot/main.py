538906a8ab0d6efa9577327f4c9cbc4f348a4bbd
MakeMeFifi <chrismikolajek2006@gmail.com>
2026-09-07 13:40:46 +0200

    Added flags to prevent bot detection

--- src/autorewardbot/main.py
+++ src/autorewardbot/main.py
@@ -2,9 +2,30 @@ from selenium import webdriver
 from rich.console import Console
 import pyfiglet
 
+
+"""
+TODO:
+    1. Login
+    2. Automating
+
+Reward Goals:
+    1. Search streak
+    2. set Streak
+    3.bing explore
+    4. more options
+    5. Dashboard free points
+
+"""
 console: Console = Console()
 title = pyfiglet.figlet_format("Auto Reward Bot", font="pagga")
-driver:webdriver.Edge = webdriver.Edge()    # Opens the edge window
+   # Opens the edge window
+options = webdriver.EdgeOptions()
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+driver:webdriver.Edge = webdriver.Edge(options=options) 
 
 
 def main():
